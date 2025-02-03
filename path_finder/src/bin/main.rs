732d344955506d267a13ff15e81a5732b94c5a2a
Bhuwan Pandit <bhuwanpandit109@gmail.com>
2025-02-03 23:28:07 +0000

    feat: add binary to check

---
+++ path_finder/src/bin/main.rs
@@ -0,0 +1,15 @@
+use std::{env, process};
+
+use path_finder::Graph;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: ./path_finder <json_file>");
+        process::exit(1);
+    }
+    let json_file_path = &args[1];
+    
+    let graph = Graph::from_json_file(&json_file_path).expect("Failed to parse json to graph");
+    println!("{:#?}", graph);
+}
