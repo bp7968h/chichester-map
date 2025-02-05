8c33ead87003fdb9cbfa18ad0dc072cb5d350cde
Bhuwan Pandit <bhuwanpandit109@gmail.com>
2025-02-05 18:33:12 +0000

    chore: update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
+import wasm from 'vite-plugin-wasm'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react()
+    react(), wasm()
   ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  optimizeDeps: {
+    exclude: ['osm_parser', 'path_finder']
+  },
+  build: {
+    target: "esnext",
+  }
 })
