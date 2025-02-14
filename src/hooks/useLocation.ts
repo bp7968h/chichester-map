07469ccd060e60e6ba690bdaf4e4b07755dce3d7
Bhuwan Pandit <bhuwanpandit109@gmail.com>
2025-02-14 18:42:45 +0000

    fix: move marker to chichester if user location is outside chichester

--- src/hooks/useLocation.ts
+++ src/hooks/useLocation.ts
@@ -14,10 +14,23 @@ export function useLocation () {
         }
 
         const success = (position: GeolocationPosition) => {
-            setLocation({
-                latitude: position.coords.latitude,
-                longitude: position.coords.longitude,
-            });
+            const {latitude, longitude } = position.coords;
+            const maxlat = 50.84156;
+            const maxlon = -0.75059;
+            const minlat = 50.8254;
+            const minlon = -0.79419;
+
+            if (latitude >= minlat && latitude <= maxlat && longitude >= minlon && longitude <= maxlon) {
+                setLocation({
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude,
+                });
+            } else {
+                setLocation({
+                    latitude: 50.8376,
+                    longitude: 0.7749,
+                });
+            }
         };
 
         const failure = (err: GeolocationPositionError) => {
